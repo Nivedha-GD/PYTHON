1f07e3496c9e24366752cdfe411e454eb6d242cc
Nivedha-GD <nanbazhagan@griddynamics.com>
2024-09-16 15:26:40 +0530

    Add files via upload

---
+++ 1_python_part_1/task1.py
@@ -0,0 +1,32 @@
+'''
+Write function which deletes defined element from list.
+Restriction: Use .pop method of list to remove item.
+Examples:
+    >>> delete_from_list([1, 2, 3, 4, 3], 3)
+    [1, 2, 4]
+    >>> delete_from_list(['a', 'b', 'c', 'b', 'd'], 'b')
+    ['a', 'c', 'd']
+    >>> delete_from_list([1, 2, 3], 'b')
+    [1, 2, 3]
+    >>> delete_from_list([], 'b')
+    []
+'''
+from typing import List, Any
+
+
+def delete_from_list(list_to_clean: List, item_to_delete: Any) -> List:
+    i=0
+    while i<len(list_to_clean):
+        if list_to_clean[i]==item_to_delete:
+            list_to_clean.pop(i)
+
+        i=i+1
+    print(list_to_clean)
+            
+            
+        
+
+delete_from_list([1, 2, 3, 4, 3], 3)
+delete_from_list(['a', 'b', 'c', 'b', 'd'], 'b')
+delete_from_list([1, 2, 3], 'b')
+delete_from_list([], 'b')
